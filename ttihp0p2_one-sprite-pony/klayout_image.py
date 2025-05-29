d2e00083868d72a05acdd0b2d8f51f2659a55aa6
Leo Moser <leomoser99@gmail.com>
2025-05-29 21:01:06 +0200

    Add design for ttihp0p2

---
+++ ttihp0p2_one-sprite-pony/klayout_image.py
@@ -0,0 +1,33 @@
+in_file = "6_final.gds"
+out_name = "one-sprite-pony_ttihp0p2"
+
+w = 2048
+aspect_ratio = 15/10
+h = w / aspect_ratio
+background_white = "#FFFFFF"
+background_black = "#000000"
+
+import klayout.lay as lay
+import klayout.db as db
+
+lv = lay.LayoutView()
+
+lv.set_config("grid-visible", "false")
+lv.set_config("grid-show-ruler", "false")
+lv.set_config("text-visible", "false")
+lv.load_layout(in_file, 0)
+lv.max_hier()
+
+lv.set_config("background-color", background_white)
+lv.save_image(out_name + "_wb.png", w, h)
+
+lv.set_config("background-color", background_black)
+lv.save_image(out_name + "_bb.png", w, h)
+
+lv.load_layer_props("sg13g2.lyp")
+
+lv.set_config("background-color", background_white)
+lv.save_image(out_name + "_wb_lyp.png", w, h)
+
+lv.set_config("background-color", background_black)
+lv.save_image(out_name + "_bb_lyp.png", w, h)
