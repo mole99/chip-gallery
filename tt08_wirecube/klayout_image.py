64c52a9423ec237cd4672eb3bc8a5948ba44d3ec
Leo Moser <leomoser99@gmail.com>
2025-05-29 21:00:49 +0200

    Add designs for TT08

---
+++ tt08_wirecube/klayout_image.py
@@ -0,0 +1,33 @@
+in_file = "tt_um_tt08_wirecube.gds"
+out_name = "tt08-wirecube"
+
+w = 2048
+aspect_ratio = 10/15
+h = w / aspect_ratio
+background_white = "#FFFFFF"
+background_black = "#000000"
+
+import klayout.lay as lay
+import klayout.db as db
+
+lv = lay.LayoutView()
+
+lv.set_config("grid-visible", "false")
+lv.set_config("grid-show-ruler", "false")
+lv.set_config("text-visible", "false")
+lv.load_layout(in_file, 0)
+lv.max_hier()
+
+lv.set_config("background-color", background_white)
+lv.save_image(out_name + "_wb.png", w, h)
+
+lv.set_config("background-color", background_black)
+lv.save_image(out_name + "_bb.png", w, h)
+
+lv.load_layer_props("sky130A.lyp")
+
+lv.set_config("background-color", background_white)
+lv.save_image(out_name + "_wb_lyp.png", w, h)
+
+lv.set_config("background-color", background_black)
+lv.save_image(out_name + "_bb_lyp.png", w, h)
